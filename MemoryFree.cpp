d9ddab08183cbd00a3e56f377f0be2a1bc561745
per1234 <per1234@users.noreply.github.com>
2015-05-25 11:28:40 -0700

    Remove semicolon from #include "MemoryFree.h";
    This causes a compiler warning.

--- MemoryFree.cpp
+++ MemoryFree.cpp
@@ -20,7 +20,7 @@ struct __freelist
 /* The head of the free list structure */
 extern struct __freelist *__flp;
 
-#include "MemoryFree.h";
+#include "MemoryFree.h"
 
 /* Calculates the size of the free list */
 int freeListSize()
